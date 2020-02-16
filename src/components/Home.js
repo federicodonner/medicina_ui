0f8ec340f043edb6383889ffa82bf70c2e0516f6
Federico Donner <federico.donner@gmail.com>
2020-02-16 00:18:19 -0300

    build(home): section layout, no functional links

--- src/components/Home.js
+++ src/components/Home.js
@@ -43,23 +43,12 @@ class Home extends React.Component {
     // Verifica si el usuario ya seleccionó el pastillero
     const user_info = verifyLogin();
     if (user_info && user_info.pastillero) {
-      // If it has, store the information in state
-      this.setState({ user_info }, function() {
-      //   // Fetch the rest of the user information
-      //   fetchActiveUser(this.state.user.token)
-      //     .then(res => res.json())
-      //     .then(activeUser => this.setState({ activeUser }, function() {}));
-      //   // Load the available books
-      //   fetchBooks(this.state.user.token, "true")
-      //     .then(res => res.json())
-      //     .then(availableBooks => this.setState({ availableBooks }));
-      });
+      // Si la tiene, la guarda en el estado
+      this.setState({ user_info }, function() {});
     } else {
       // Si no hay data en localstorage, va a la pantalla de selección de pastillero
-      // this.props.history.push(`/userselect`);
       this.props.history.push({
         pathname: "/seleccionarPastillero"
-        //state: { prueba: "hoooola" }
       });
     }
   }
@@ -68,114 +57,28 @@ class Home extends React.Component {
     return (
       <div className="app-view cover">
         <div className="scrollable">
-          {this.state && this.state.user_info && (
-            <Header/>
-          )}
+          {this.state && this.state.user_info && <Header />}
           <div className="content">
-            {this.state && !this.state.activeUser && (
-              <p>
-                <img className="loader" src="/images/loader.gif" />
-              </p>
-            )}
-
-            {this.state &&
-              this.state.activeUser &&
-              this.state.activeUser.libros.length == 0 && (
-                <p>
-                  Para poder sumarte al Libroclub de tu grupo es necesario que
-                  traigas por lo menos un libro. Podés ingresarlos{" "}
-                  <a onClick={this.goToAddBook()}>haciendo click aquí</a>.
-                </p>
-              )}
-
-            {this.state &&
-              this.state.activeUser &&
-              this.state.activeUser.libros.length > 0 && (
-                <>
-                  {this.state &&
-                    this.state.activeUser &&
-                    !this.state.activeUser.alquilerActivo && (
-                      <p>
-                        No tenés ningún libro alquilado.
-                        <span className="newLine">
-                          Estos están disponibles para que te lleves:
-                        </span>
-                      </p>
-                    )}
-
-                  {this.state &&
-                    this.state.activeUser &&
-                    this.state.activeUser.alquilerActivo && (
-                      <>
-                        <p>
-                          Tenes{" "}
-                          <BookName
-                            id={this.state.activeUser.alquilerActivo.id_libro}
-                            name={
-                              this.state.activeUser.alquilerActivo.nombre_libro
-                            }
-                            navigation={this.props.history}
-                          />{" "}
-                          desde el{" "}
-                          {convertDate(
-                            this.state.activeUser.alquilerActivo.fecha_salida
-                          )}
-                          .
-                          <span className="newLine">
-                            Si ya lo devolviste,{" "}
-                            <a onClick={this.goToReturnBook}>
-                              finaliza el alquiler
-                            </a>
-                            .
-                          </span>
-                        </p>
-                        <img
-                          className="separador"
-                          src="/images/separador.png"
-                        />
-                        <p>
-                          Estos son los libros que podés disfrutar cuando
-                          termines{" "}
-                          <BookName
-                            id={this.state.activeUser.alquilerActivo.id_libro}
-                            name={
-                              this.state.activeUser.alquilerActivo.nombreLibro
-                            }
-                            navigation={this.props.history}
-                          />
-                          :
-                        </p>
-                      </>
-                    )}
-
-                  {this.state && this.state.availableBooks && (
-                    <ul className="libros">
-                      {this.state.availableBooks.map(obj => {
-                        return (
-                          <li key={obj.id}>
-                            <BookName
-                              id={obj.id}
-                              name={obj.titulo}
-                              navigation={this.props.history}
-                            />{" "}
-                            - {obj.autor}
-                          </li>
-                        );
-                      })}
-                    </ul>
-                  )}
-                  <img className="separador" src="/images/separador.png" />
-                  <p>
-                    {" "}
-                    Ver mis{" "}
-                    <a onClick={this.goToPersonalDetails}>datos personales</a>
-                  </p>
-                  <img className="separador" src="/images/separador.png" />
-                  <p>
-                    <a onClick={this.signOut}>Salir de Libroclub</a>
-                  </p>
-                </>
-              )}
+            <div className="nav-buttons">
+              <div className="nav-button">
+                <div className="nav-icon nav-icon-ver-dosis"></div>
+                <span className="single-line">ver</span>
+                <span>mis dosis</span>
+              </div>
+              <div className="nav-button">
+                <div className="nav-icon nav-icon-editar-dosis"></div>
+                <span className="single-line">editar</span>
+                <span>mis dosis</span>
+              </div><div className="nav-button">
+                <div className="nav-icon nav-icon-consultar-stock"></div>
+                <span className="single-line">consultar</span>
+                <span>stock</span>
+              </div><div className="nav-button">
+                <div className="nav-icon nav-icon-ingresar-compra"></div>
+                <span className="single-line">ingresar</span>
+                <span>compra</span>
+              </div>
+            </div>
           </div>
         </div>
       </div>

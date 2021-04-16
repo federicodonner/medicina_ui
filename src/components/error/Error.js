455e97018e838d6ad7d30dca4b2aa016a93d13bf
federicodonner <federico.donner@gmail.com>
2021-04-16 19:25:59 -0300

    refactor(Error): convertir en componente funcional

--- src/components/error/Error.js
+++ src/components/error/Error.js
@@ -1,37 +1,27 @@
 import React from "react";
 import "./error.css";
 
-class Error extends React.Component {
-  state: {
-    userInfo: {},
-  };
-
-  navegarAHome = () => (event) => {
-    this.props.history.push({
+export default function Error(props) {
+  function navegarAHome() {
+    props.history.push({
       pathname: "home",
     });
-  };
+  }
 
-  componentDidMount() {}
-
-  render() {
-    return (
-      <div className="app-view cover">
-        <div className="scrollable">
-          <div className="content">
-            <div className="imagen-error" />
-            <p>
-              Algo funcionó mal, te pedimos disculpas. Presiona{" "}
-              <span className="negrita" onClick={this.navegarAHome()}>
-                aquí
-              </span>{" "}
-              para volver.
-            </p>
-          </div>
+  return (
+    <div className="app-view cover">
+      <div className="scrollable">
+        <div className="content">
+          <div className="imagen-error" />
+          <p>
+            Algo funcionó mal, te pedimos disculpas. Presiona{" "}
+            <span className="negrita" onClick={navegarAHome}>
+              aquí
+            </span>{" "}
+            para volver.
+          </p>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
-
-export default Error;

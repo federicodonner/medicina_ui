8824011c858ee8b1bfeb39008cd52663b2d89826
federicodonner <federico.donner@gmail.com>
2021-04-16 18:41:06 -0300

    refactor(Login): implementar hooks

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,204 +1,169 @@
-import React from "react";
+import React, { useState, useRef, useEffect } from "react";
 import Header from "../header/Header";
 import { guardarEnLS, accederAPI } from "../../utils/fetchFunctions";
 import "./login.css";
 import variables from "../../var/variables.js";
 
-class Login extends React.Component {
-  state: {
-    loader: {},
-    formIngresada: {},
-  };
+export default function Login(props) {
+  const [loader, setLoader] = useState(true);
+  const [loaderTexto, setLoaderTexto] = useState("Verificando login.");
+  const [formIngresada, setFormIngresada] = useState(false);
 
-  loginRef = React.createRef();
-  passwordRef = React.createRef();
+  const [userInfo, setUserInfo] = useState(null);
 
-  navigateToSection = (section, data) => (event) => {
-    event.preventDefault();
-    this.props.history.push(
-      {
-        pathname: section,
-      },
-      data
-    );
-  };
+  const loginRef = useRef(null);
+  const passwordRef = useRef(null);
 
-  // Función utilizada para actualizar el state y esconder
-  // el cartel de falta de input cuando el usuario ingresa algo
-  actualizarState = () => () => {
-    if (this.state) {
-      var formIngresada = this.state.formIngresada;
-      this.setState({ formIngresada });
-    }
-  };
-
-  // prende el loader antes de cargar el componente
-  // formIngresada indica si se deben mostrar alertas del formulario de login
-  constructor(props) {
-    super(props);
-    this.state = {
-      loader: {
-        encendido: true,
-        texto: "Verificando login.",
-      },
-      formIngresada: false,
-    };
-  }
-
-  // Callback de la llamada a la API cuando el estado es 200
-  usuarioLogueado = (userInfo) => {
-    // Si entra en esta función es porque el usuario ya estaba logueado
-    // lo manda a home
-    this.props.history.push({ pathname: "/home" }, { userInfo: userInfo });
-  };
-
-  // callback de la llamada a la API cuando el estado no es 200
-  usuarioNoLogueado = (datos) => {
-    // Error 500+ es un error de la API, lo manda a la pantalla del error
-    if (datos.status >= 500) {
-      this.props.history.push("error");
-      // Si el error es de otros tipos, muestra el mensaje de error y apaga el loader
-    } else {
-      this.setState({ loader: { encendido: false } });
-    }
-  };
-
-  // Callback de post a oauth correcto
-  loginCorrecto = (data) => {
-    guardarEnLS(variables.LSLoginToken, data.token);
-    this.props.history.push({
-      pathname: "home",
-    });
-  };
-
-  // Callback de error de post a oauth
-  errorDeLogin = (data) => {
-    // Verifica si es un error de la API o un problema de oauth
-    if (data.status >= 500) {
-      alert("Ocurrió un error, por favor inténtalo denuevo más tarde.");
-    } else {
-      alert(data.detail);
-      this.setState({ loader: { encendido: false }, formIngresada: false });
-    }
-  };
-
-  componentDidMount() {
+  useEffect(() => {
     // Va a buscar los datos del usuario
     // Si los encuentra, entra a la app y le pasa los datos al componente
     accederAPI(
       "GET",
       "usuario",
       null,
-      this.usuarioLogueado,
-      this.usuarioNoLogueado
+      () => {
+        navigateToSection("home");
+      },
+      usuarioNoLogueado
     );
+  }, []);
+
+  function navigateToSection(section) {
+    props.history.push({
+      pathname: section,
+    });
   }
 
-  submitLogin = (event) => {
-    event.preventDefault();
+  // callback de la llamada a la API cuando el estado no es 200
+  function usuarioNoLogueado(datos) {
+    // Error 500+ es un error de la API, lo manda a la pantalla del error
+    if (datos.status >= 500) {
+      this.props.history.push("error");
+      // Si el error es de otros tipos, muestra el mensaje de error y apaga el loader
+    } else {
+      setLoader(false);
+    }
+  }
 
+  // Callback de error de post a oauth
+  function errorDeLogin(data) {
+    // Verifica si es un error de la API o un problema de oauth
+    if (data.status >= 500) {
+      alert("Ocurrió un error, por favor inténtalo denuevo más tarde.");
+    } else {
+      alert(data.detail);
+      setLoader(false);
+      setFormIngresada(false);
+    }
+  }
+
+  function submitLogin(e) {
+    e.preventDefault();
     // Indica que fue ingresado el formulario
-    this.setState({
-      formIngresada: true,
-    });
+    setFormIngresada(true);
 
     // Verifica que se hayan ingresados ambos campos
-    if (this.loginRef.current.value && this.passwordRef.current.value) {
-      // Prende el loader
-      this.setState({
-        loader: {
-          encendido: true,
-          texto: "Enviando información de login.",
-        },
-      });
+    if (loginRef.current.value && passwordRef.current.value) {
+      setLoaderTexto("Enviando información de login.");
+      setLoader(true);
+
       // Genera el objeto de datos para el login
       var data = {
-        username: this.loginRef.current.value,
-        access: this.passwordRef.current.value,
+        username: loginRef.current.value,
+        access: passwordRef.current.value,
         grant_type: "password",
       };
 
-      accederAPI("POST", "oauth", data, this.loginCorrecto, this.errorDeLogin);
+      accederAPI(
+        "POST",
+        "oauth",
+        data,
+        (respuesta) => {
+          guardarEnLS(variables.LSLoginToken, respuesta.token);
+          navigateToSection("home");
+        },
+        errorDeLogin
+      );
     }
-  };
-
-  render() {
-    return (
-      <div className="app-view cover">
-        <div className="scrollable">
-          {this.state && this.state.loader.encendido && (
-            <div className="loader-container">
-              <p>
-                <img className="loader" src="/images/loader.svg" />
-              </p>
-              <p className={"negrita"}>{this.state.loader.texto}</p>
-            </div>
-          )}
-          {this.state && !this.state.loader.encendido && (
-            <>
-              <Header />
-              <div className="content">
-                <p>
-                  Bienvenido a MiDosis la plataforma online de gestión de
-                  medicamentos.
-                </p>
-                <form onSubmit={this.submitLogin}>
-                  <div className={"login-form"}>
-                    <span className="label">Nombre de usuario:</span>
-                    <input
-                      rows="8"
-                      name="nombre"
-                      type="text"
-                      ref={this.loginRef}
-                      className="login-input"
-                      onChange={this.actualizarState()}
-                    />
-                    {this.state &&
-                      this.state.formIngresada &&
-                      this.loginRef.current &&
-                      !this.loginRef.current.value && (
-                        <span className="login-error">
-                          Debes completar tu nombre de usuario.
-                        </span>
-                      )}
-                  </div>
-                  <div className={"login-form"}>
-                    <span className="label">Contraseña:</span>
-                    <input
-                      rows="8"
-                      name="nombre"
-                      type="password"
-                      ref={this.passwordRef}
-                      className="login-input"
-                      onChange={this.actualizarState()}
-                    />
-                    {this.state &&
-                      this.state.formIngresada &&
-                      this.passwordRef.current &&
-                      !this.passwordRef.current.value && (
-                        <span className="login-error">
-                          Debes completar tu contraseña.
-                        </span>
-                      )}
-                  </div>
-                  <button className="login-submit" type="submit">
-                    Ingresar
-                  </button>
-                </form>
-                <div className={"pretty-olvido"}>Olvidé mi contraseña</div>
-                <div
-                  className={"pretty-olvido"}
-                  onClick={this.navigateToSection("crearCuenta", null)}
-                >
-                  Crear una cuenta
-                </div>
-              </div>
-            </>
-          )}
-        </div>
-      </div>
-    );
   }
-}
 
-export default Login;
+  function apagarErrores() {
+    setFormIngresada(false);
+  }
+
+  return (
+    <div className="app-view cover">
+      <div className="scrollable">
+        {loader && (
+          <div className="loader-container">
+            <p>
+              <img className="loader" src="/images/loader.svg" />
+            </p>
+            <p className={"negrita"}>{loaderTexto}</p>
+          </div>
+        )}
+        {!loader && (
+          <>
+            <Header />
+            <div className="content">
+              <p>
+                Bienvenido a MiDosis la plataforma online de gestión de
+                medicamentos.
+              </p>
+              <form onSubmit={submitLogin}>
+                <div className={"login-form"}>
+                  <span className="label">Nombre de usuario:</span>
+                  <input
+                    rows="8"
+                    name="nombre"
+                    type="text"
+                    ref={loginRef}
+                    onChange={apagarErrores}
+                    className="login-input"
+                  />
+                  {formIngresada &&
+                    loginRef.current &&
+                    !loginRef.current.value && (
+                      <span className="login-error">
+                        Debes completar tu nombre de usuario.
+                      </span>
+                    )}
+                </div>
+                <div className={"login-form"}>
+                  <span className="label">Contraseña:</span>
+                  <input
+                    rows="8"
+                    name="nombre"
+                    type="password"
+                    ref={passwordRef}
+                    onChange={apagarErrores}
+                    className="login-input"
+                  />
+                  {formIngresada &&
+                    passwordRef.current &&
+                    !passwordRef.current.value && (
+                      <span className="login-error">
+                        Debes completar tu contraseña.
+                      </span>
+                    )}
+                </div>
+                <button className="login-submit" type="submit">
+                  Ingresar
+                </button>
+              </form>
+              <div className={"pretty-olvido"}>Olvidé mi contraseña</div>
+              <div
+                className={"pretty-olvido"}
+                onClick={() => {
+                  navigateToSection("crearCuenta");
+                }}
+              >
+                Crear una cuenta
+              </div>
+            </div>
+          </>
+        )}
+      </div>
+    </div>
+  );
+}
